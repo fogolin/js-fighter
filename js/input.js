c25c2d7166bc7f85dc2bd80497019bbb01c1f4e5
Lucas Fogolin <lucasfogolin@gmail.com>
2025-12-01 14:12:39 -0300

    ⚰️Remove template code.

--- js/input.js
+++
@@ -1,8 +0,0 @@
-// central input module (exports key state)
-const keys = {};
-window.addEventListener('keydown', e => keys[e.key.toLowerCase()] = true);
-window.addEventListener('keyup', e => keys[e.key.toLowerCase()] = false);
-
-export function setupInput() { /* left placeholder in case we want init logic */ }
-export function isKey(k) { return !!keys[k.toLowerCase()]; }
-export function getKeys() { return keys; }
