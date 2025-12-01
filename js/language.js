4fd71643565e3bd4f87e39fa35f5b282f128e46a
Lucas Fogolin <lucasfogolin@gmail.com>
2025-12-01 14:13:15 -0300

    🎨Extract language ystem to external files.

---
+++ js/language.js
@@ -0,0 +1,8 @@
+// import languages
+import pob from './languages/pob.js';
+
+export const language = {
+    pob
+}
+
+export default language;
---
+++ js/languages/pob.js
@@ -0,0 +1,39 @@
+export const language = {
+    titleScreen: {
+        title: "Grupon Fighter",
+        subtitle: "A capivara gritante",
+        insertCoin: "Inserir ficha para começar",
+        footer: "co.,ltd. 2025"
+    },
+    characterSelection: {
+        title: "Selecione os lutadores",
+        subtitle: "< A / D > ou < Setas > para mudar",
+        player: "Jogador",
+        instructions: {
+            p1: "A / D",
+            p2: "Setas",
+            continue: "Pressione ENTER para confirmar"
+        },
+        oponent: {
+            mode: "Tipo de adversário (Espaço)",
+            human: "Humano",
+            ai: "CPU"
+        }
+    },
+    stageSelection: {
+        title: "Selecione a fase",
+        subtitle: "< A / D > ou < Setas > para mudar",
+        pressEnter: "Pressione <strong>ENTER</strong> para lutar!",
+    },
+    hud: {
+        fight: "Lutem!",
+        round: "Rodada",
+        ko: "K.O.",
+        win: "Vitória!",
+        wins: "vence!",
+        draw: "Empate",
+        timeOver: "Tempo esgotado!"
+    }
+}
+
+export default language;
